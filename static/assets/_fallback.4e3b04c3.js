6fec4afe3d9c091a335d00e5d95f7264006a63df
kwagi <heisenberg123457@gmail.com>
2023-05-05 01:15:23 +0900

    git

---
+++ static/assets/_fallback.4e3b04c3.js
@@ -0,0 +1,3 @@
+import{S as m,i as b,s as g,e as p,a as h,t as d,b as o,c as x,d as s,n as _,f as k,g as q,u as $}from"./index.e5a5012b.js";function y(r){let e,a,l,t,f,n,v,c;return{c(){e=p("div"),a=p("div"),a.textContent="404",l=h(),t=p("div"),f=d(`Page not found. 
+  
+  `),n=p("a"),v=d("Go back"),o(a,"class","huge svelte-viq1pm"),o(n,"href",c=r[0]("../")),o(t,"class","big"),o(e,"class","e404 svelte-viq1pm")},m(i,u){x(i,e,u),s(e,a),s(e,l),s(e,t),s(t,f),s(t,n),s(n,v)},p(i,[u]){u&1&&c!==(c=i[0]("../"))&&o(n,"href",c)},i:_,o:_,d(i){i&&k(e)}}}function C(r,e,a){let l;return q(r,$,t=>a(0,l=t)),[l]}class F extends m{constructor(e){super(),b(this,e,C,y,g,{})}}export{F as default};
